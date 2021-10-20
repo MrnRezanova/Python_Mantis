2c24c982ff5a83626cecd30a34b5b64a24bc3314
Marina <m.rezanova@gemark.ru>
2021-10-20 16:52:27 +0300

    баг-трекер Mantis

---
+++ fixture/application.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+
+class Application:
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.wd.implicitly_wait(1)
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get(self.base_url)
+
+    def change_field_value(self, field_name, text):
+        wd = self.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def return_to_home_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("home page").click()
+
+    def destroy(self):
+        self.wd.quit()
