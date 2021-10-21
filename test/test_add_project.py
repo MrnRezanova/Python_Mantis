d7509848d450986628fb3484dd7f748c196a6041
Marina <m.rezanova@gemark.ru>
2021-10-21 13:18:20 +0300

    Создание и удаление проекта

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -12,7 +12,6 @@ testdata = Project(name=random_string("name", 6))
 
 
 def test_add_project(app):
-    app.session.login("administrator", "root")
     old_projects = app.project.get_project_list()
     app.project.create(testdata)
     new_projects = app.project.get_project_list()
--- test/test_delete_project.py
+++ test/test_delete_project.py
@@ -1,7 +1,6 @@
 from model.project import Project
 
 def test_delete_project(app):
-    app.session.login("administrator", "root")
     print(app.project.get_project_list())
     if app.project.get_project_list() == 1:
         app.project.create(Project(name="NewProject"))
